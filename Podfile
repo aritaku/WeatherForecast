4a8e35eee9b4c1502a75cf7fff95ab2d1d3c896f
有村琢磨 <arimuratakuma@aritaku.local>
2016-02-08 19:31:58 +0900

    [add] Libraries

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'WeatherForecast' do
+pod 'Alamofire', '~> 3.1.5'
+pod 'RealmSwift', '~> 0.97.1'
+pod 'SwiftyJSON', '~> 2.3.2'
+end
+
+target 'WeatherForecastTests' do
+
+end
+
+target 'WeatherForecastUITests' do
+
+end
---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by 有村琢磨 on 2016/02/08.
+//  Copyright © 2016年 有村琢磨. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
